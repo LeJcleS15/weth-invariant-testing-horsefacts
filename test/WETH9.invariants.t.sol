b1fd385e2cd2444af68caa0c61715fa385748ff2
0xdineshkumarsm <105421120+0xdineshkumarsm@users.noreply.github.com>
2023-03-17 13:29:57 +0530

    adding handler

--- test/WETH9.invariants.t.sol
+++ test/WETH9.invariants.t.sol
@@ -4,18 +4,36 @@ import {Test} from "forge-std/Test.sol";
 // import {InvariantTest} from "forge-std/InvariantTest.sol";
 import {WETH9} from "../src/WETH9.sol";
 
+import {Handler} from "./handlers/Handler.sol";
 contract WETH9Invariants is Test {
     WETH9 public weth;
 
+    Handler public handler;
+
+
     function setUp() public {
         weth = new WETH9();
+        handler = new Handler(weth);
+
+        targetContract(address(handler));
     }
 
-    function invariant_wethSupplyIsAlwaysZero() public {
-        assertEq(0, weth.totalSupply());
-    }
+    // function invariant_wethSupplyIsAlwaysZero() public {
+    //     assertEq(0, weth.totalSupply());
+    // }
 
     // function invariant_badInvariantThisShouldFail() public {
     //     assertEq(1, weth.totalSupply());
     // }
+
+    // ETH can only be wrapped into WETH, WETH can only
+    // be unwrapped back into ETH. The sum of the Handler's
+    // ETH balance plus the WETH totalSupply() should always
+    // equal the total ETH_SUPPLY.
+    function invariant_conservationOfETH() public {
+        assertEq(
+          handler.ETH_SUPPLY(),
+          address(handler).balance + weth.totalSupply()
+        );
+    }
 }
---
+++ test/handlers/Handler.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.13;
+
+import {CommonBase} from "forge-std/Base.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
+import {StdUtils} from "forge-std/StdUtils.sol";
+
+import {WETH9} from "../../src/WETH9.sol";
+
+contract Handler is CommonBase, StdCheats, StdUtils {
+    WETH9 public weth;
+
+    uint256 public constant ETH_SUPPLY = 120_500_000 ether;
+
+    constructor(WETH9 _weth) {
+        weth = _weth;
+        deal(address(this), ETH_SUPPLY);
+
+    }
+
+    function deposit(uint256 amount) public {
+        amount = bound(amount, 0, address(this).balance);
+        weth.deposit{ value: amount }();
+    }
+}
